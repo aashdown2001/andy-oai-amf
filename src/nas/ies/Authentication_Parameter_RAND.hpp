2921bc4d317b57e1f71984d0a1211c346d0b142b
Tien Thinh NGUYEN <Tien-Thinh.Nguyen@eurecom.fr>
2023-01-11 18:26:22 +0100

    Update Authentication_Parameter_RAND

--- src/nas/ies/Authentication_Parameter_RAND.hpp
+++ src/nas/ies/Authentication_Parameter_RAND.hpp
@@ -19,34 +19,33 @@
  *      contact@openairinterface.org
  */
 
-/*! \file
- \brief
- \author  Keliang DU, BUPT
- \date 2020
- \email: contact@openairinterface.org
- */
+#ifndef _AUTHENTICATION_PARAMETER_RAND_H_
+#define _AUTHENTICATION_PARAMETER_RAND_H_
 
-#ifndef __Authentication_Parameter_RAND_H_
-#define __Authentication_Parameter_RAND_H_
+#include "Type3NasIe.hpp"
 
 #include <stdint.h>
 
+constexpr uint8_t kAuthenticationParameterRandLength = 17;
+constexpr auto kAuthenticationParameterRandIeName =
+    "Authentication Parameter RAND";
+
 namespace nas {
 
-class Authentication_Parameter_RAND {
+class Authentication_Parameter_RAND : public Type3NasIe {
  public:
   Authentication_Parameter_RAND();
   Authentication_Parameter_RAND(uint8_t iei);
-  Authentication_Parameter_RAND(const uint8_t iei, uint8_t* value);
+  Authentication_Parameter_RAND(uint8_t iei, uint8_t* value);
   ~Authentication_Parameter_RAND();
-  // void setValue(uint8_t iei, uint8_t value);
+
   int Encode(uint8_t* buf, int len);
   int Decode(uint8_t* buf, int len, bool is_option);
-  uint8_t* getValue();
+
+  // uint8_t* getValue();
 
  private:
-  uint8_t _iei;
-  uint8_t _value[16];
+  uint8_t _value[kAuthenticationParameterRandLength - 1];
 };
 
 }  // namespace nas
